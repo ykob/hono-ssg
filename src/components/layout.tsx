70c3b03abce49d8ccd593fc2789a7c59eff7a4cd
ykob <info@tplh.net>
2024-10-08 18:18:19 +0900

    update meta tags.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,6 +9,8 @@ export function Layout({ children, title }: LayoutProps) {
     <html>
       <head>
         <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>{children}</body>
     </html>
