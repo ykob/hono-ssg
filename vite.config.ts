5223b85075fd1a1c3f79fe55541765877682f5cb
ykob <info@tplh.net>
2024-10-08 17:49:29 +0900

    update entry point path.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import devServer from '@hono/vite-dev-server';
 import ssg from '@hono/vite-ssg';
 import { defineConfig } from 'vite';
 
-const entry = 'src/index.tsx';
+const entry = 'src/index.ts';
 
 export default defineConfig({
   plugins: [devServer({ entry }), ssg({ entry })],
