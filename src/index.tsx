014328fad20885b893a08ac928aac6e89c99efbf
ykob <info@tplh.net>
2024-10-10 14:13:21 +0900

    added remark and rehype.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
 import fs from 'fs';
 import { Hono } from 'hono';
+import rehypeStringify from 'rehype-stringify';
+import { remark } from 'remark';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
 import { Layout } from './components';
 
 // index.tsx
 const app = new Hono();
 
-app.get('/', (c) => {
-  const content = fs.readFileSync('./src/index.md', 'utf-8');
+const processor = remark()
+  .use(remarkParse)
+  .use(remarkRehype, { allowDangerousHtml: true })
+  .use(rehypeStringify, { allowDangerousHtml: true });
 
-  return c.html(<Layout title="Hello, World Title">{content}</Layout>);
+app.get('/', async (c) => {
+  const file = fs.readFileSync('./src/index.md', 'utf-8');
+  const content = await processor.process(file);
+
+  return c.html(
+    <Layout title="Hello, World Title">
+      <div dangerouslySetInnerHTML={{ __html: String(content) }} />
+    </Layout>,
+  );
 });
 
 export default app;
