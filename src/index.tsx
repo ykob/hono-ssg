7b20dbdb985c7b6c2a965dd22db3184019b30a63
ykob <info@tplh.net>
2024-10-13 16:51:29 +0900

    removed remark-parse.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { remark } from 'remark';
 import remarkExtractFrontmatter from 'remark-extract-frontmatter';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
-import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import yaml from 'yaml';
 import { Layout } from './components';
@@ -13,12 +12,11 @@ import { Layout } from './components';
 // index.tsx
 const app = new Hono();
 const processor = remark()
-  .use(remarkParse)
   .use(remarkFrontmatter)
   .use(remarkExtractFrontmatter, { yaml: yaml.parse })
   .use(remarkGfm)
-  .use(remarkRehype, { allowDangerousHtml: true })
-  .use(rehypeStringify, { allowDangerousHtml: true });
+  .use(remarkRehype)
+  .use(rehypeStringify);
 
 app.get('/', async (c) => {
   const markdown = fs.readFileSync('./src/index.md', 'utf-8');
