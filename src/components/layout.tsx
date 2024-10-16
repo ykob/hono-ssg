3441ee3b5db04fd400b6398051931b97462adaad
ykob <info@tplh.net>
2024-10-16 19:26:48 +0900

    added global styles.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,6 @@
+import { Style } from 'hono/css';
 import { PropsWithChildren } from 'hono/jsx';
+import { globalStyles } from '../styles/global';
 
 type LayoutProps = PropsWithChildren<{
   description: string;
@@ -13,8 +15,9 @@ export function Layout({ children, description, title }: LayoutProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={description} />
+        <Style />
       </head>
-      <body>{children}</body>
+      <body class={globalStyles}>{children}</body>
     </html>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { css } from 'hono/css';
+
+export const globalStyles = css`
+  :-hono-global {
+    body {
+      margin: 0;
+      padding: 0;
+      background-color: #f0f0f0;
+    }
+  }
+`;
