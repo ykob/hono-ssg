5ba431e736fcde52c73b283cbe8cf25782a77bdf
ykob <info@tplh.net>
2024-11-23 23:46:53 +0900

    added article list item styles.

--- src/components/article-list-item.tsx
+++ src/components/article-list-item.tsx
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { css } from 'hono/css';
 import { PropsWithChildren } from 'hono/jsx';
 
 type ArticleListItemProps = PropsWithChildren<{
@@ -17,10 +18,22 @@ export function ArticleListItem({
   const Heading = 'h2';
 
   return (
-    <a href={`/posts/${id}/`}>
+    <a class={styles.container} href={`/posts/${id}/`}>
       <Heading>{title}</Heading>
       <p>{dayjs(date).format('YYYY/MM/DD')}</p>
       <p>{description}</p>
     </a>
   );
 }
+
+const styles = {
+  container: css`
+    display: block;
+    box-sizing: border-box;
+    padding: 24px 20px;
+    color: #333;
+    text-decoration: none;
+    background-color: #fff;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
+  `,
+};
